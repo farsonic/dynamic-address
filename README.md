2676e1bd56b2105a80105563dbdf58869a1a38df
farsonic <fp@prowsehouse.com>
2015-04-10 13:09:27 +1000

    initial update with dummy configus

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dynamic-address
+SRX Firewall - Dynamic creation of address books
+================================================
+The SRX firewall can be configured to monitor a central web server for a list of IP Addresses to create a dynamic address book. When the SRX is referencing this service a new address 
+book is created for each "feed" that is provided by the server. Each feed is turned into an address-book of the same name that is referenced directly within security policy, in much
+the same way static address booked are utilized today. 
+
+The important destination here is that by utilizing a dynamic feed there is no requirement to "commit" any changes to the configuration, the SRX simply applies policy based on the 
+dynamic nature of the feed it is installing. 
+
+Installation
+============
+
+
+
+
+
+
+
+```
+* MAC Address of the user 
+* Assigned IP address
+* Client Identifier (if know, otherwise *)
+* Requested hostname for resolution 
+```
--- dynamic-policy.py
+++ dynamic-policy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from xml.etree import ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+
 import time
 import sys
 import re
@@ -10,9 +14,22 @@ import hashlib
 import shutil
 import json   
 from pprint import pprint
+from jnpr.junos import Device
+import getpass
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+
+
+
 
 feed_location='/var/www/'
 temp_location='/var/tmp/'
+ 
+
+def prettify(elem):
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
 
 def modify_file(file_name,pattern,value=""):
     fh=fileinput.input(file_name,inplace=True)
@@ -39,7 +56,7 @@ def delete_line(pattern):
 
 def update_manifest(feedname):
    ts = int(time.time())
-   print "Modifying address entires for  -> " + feedname 
+   print "Modifying total number of objects for  -> " + feedname 
    with open(tempFeed,'r') as feed:
      count = sum(1 for line in feed) - 5
      print "Revised address count of       -> " + str(count) + "\n"
@@ -56,6 +73,26 @@ def update_manifest(feedname):
 	    feed.set('objects',str(count))
    
    tree.write(tempManifest) 
+
+
+def create_manifest_entry(feedname): 
+    ts = str(int(time.time()))
+    print "Inserting new feed into manifest file located at " + feed_location
+
+    tree = ET.parse(tempManifest)
+    root = tree.getroot()
+
+    category = root.find('category')
+    feed = ET.SubElement(category,'feed',dict(data_ts=ts, name=feedname, objects="0", options="", types="ip_addr ip_range", version=feedname))
+    data = ET.SubElement(feed,'data')
+    url = ET.SubElement(data,'url')
+    url.text='/'
+
+    text=(prettify(root))
+    cleantext= "".join([s for s in text.strip().splitlines(True) if s.strip()])
+
+    with open(tempManifest,'w') as file: 
+      file.write(cleantext)
 
 
 
@@ -78,6 +115,10 @@ def copy_tempManifest_to_Manifest():
 
 def copy_Manifest_to_tempManifest():
     shutil.copyfile(manifest,tempManifest)
+ 
+def create_newFeed(name):
+    shutil.copyfile('Feed',name)
+
 
 def calculate_md5():
     with open(tempFeed) as file:
@@ -133,9 +174,6 @@ if sys.argv[1]=='del':
    adj_size = size -1 
    value = ip.value
    print "\nRemoving address of            -> " + str(sys.argv[3]) +" (including " + str(adj_size) + " subequent hosts)"
-   #print address
-   #print ip.size
-   #print adj_size
 
    if adj_size == 0:
      oldline = '{"1":' + str(value) +'}'
@@ -176,4 +214,46 @@ if sys.argv[1]=='list':
     
 
 if sys.argv[1]=='new':
-   print "This is next on the list...."
+    name = str(sys.argv[2])
+    feed=feed_location + str(sys.argv[2])
+    manifest=feed_location+'manifest.xml'
+    tempManifest=temp_location+'manifest.xml'
+    copy_Manifest_to_tempManifest()
+    print name
+    create_newFeed(feed)
+    create_manifest_entry(name)
+    copy_tempManifest_to_Manifest()
+    #print "Completed, add the following line to your SRX to accept feed:\n set security dynamic-address address-name "+name+ " profile category IPFilter feed "+name 
+    username = raw_input("Please enter your SRX Username:")
+    password = getpass.getpass()
+    srx_list = 'srx-list'
+    srxs = open(srx_list,'r') 
+    for srx in srxs: 
+     print "Logging into SRX "+srx
+     login=str(srx) 
+     dev = Device(host=login,user=username,password=password)
+     dev.open()
+     dev.timeout = 300
+     cu = Config(dev)
+     set_cmd = 'set security dynamic-address address-name '+name+' profile category IPFilter feed '+name
+     cu.load(set_cmd, format='set')
+     print "Applying changes, please wait...."
+     cu.commit()
+     dev.close()
+     
+     
+     
+     
+
+
+
+
+if sys.argv[1]=='drop':
+    name = feed_location + str(sys.argv[2])
+    print name
+    print "do this next...."
+
+
+if sys.argv[1]=='setup':
+    print "Kick off initial setup process, copy files to target directories etc" 
+
